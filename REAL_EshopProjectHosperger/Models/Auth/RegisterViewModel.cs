39271dc03292a9fe3ccf2d950a2886227affa919
Matěj Hosperger <hospergermatej@sssvt.cz>
2025-03-17 22:06:27 +0100

    l

--- REAL_EshopProjectHosperger/Models/Auth/RegisterViewModel.cs
+++ REAL_EshopProjectHosperger/Models/Auth/RegisterViewModel.cs
@@ -11,21 +11,21 @@ namespace REAL_EshopProjectHosperger.Models.Auth
 
         [Required]
         [MaxLength(128)]
-        [RegularExpression("@^[a-zA-Z0-9._-]{3,20}$")]
+        
         public string Password { get; set; }
 
         [Required]
         [MaxLength(128)]
-        [RegularExpression("^[A-ZÁČĎÉĚÍŇÓŘŠŤÚŮÝŽ][a-záčďéěíňóřšťúůýž]{1,49}$")]
+        
         public string Firstname { get; set; }
         
         [Required]
-        [RegularExpression("^[A-ZÁČĎÉĚÍŇÓŘŠŤÚŮÝŽ][a-záčďéěíňóřšťúůýž]{1,49}$")]
+
         [MaxLength(128)]
         public string Lastname { get; set; }
 
-        [RegularExpression("@^[a-zA-Z0-9]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")]
-         public string? Email { get; set; }
+        [MaxLength(128)]
+        public string? Email { get; set; }
 
         public RegisterViewModel()
         {
